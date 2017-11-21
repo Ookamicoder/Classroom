78b83891a61aa64310dee5c64d951db8f6994324
Erik Edlund <erik_edlund@hotmail.se>
2017-11-21 13:01:25 +0100

    Added more test cases to the method StudentTest

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -8,6 +8,7 @@ public class StudentTest {
 
 	@Test
 	public void testGettersAndSetters() {
+		Student student2 = new Student("Christer","Edlund",68,'M');
 		Student student = new Student("Erik", "Edlund", 26, 'M', 78, 65, 100);
 		student.setFirstGrade(70.0);
 		student.setSecondGrade(86.0);
@@ -15,6 +16,10 @@ public class StudentTest {
 		assertEquals(student.getFirstGrade(),70.0,0);
 		assertEquals(student.getSecondGrade(),86.0,0);
 		assertEquals(student.getThirdGrade(),93.0,0);
+		assertEquals(student2.getFirstName().equals("Christer"),true);
+		assertEquals(student2.getLastName().equals("Edlund"),true);
+		assertEquals(student2.getAge(),68);
+		assertEquals(student2.getGender(),'M');
 		
 	}
 	
@@ -28,8 +33,11 @@ public class StudentTest {
 		student.setThirdGrade(93.0);
 		sumOfGrades = student.getFirstGrade()+student.getSecondGrade()+student.getThirdGrade();
 		averageResult = sumOfGrades/3;
+		student.calculateAverage(student.getFirstGrade());
+		student.calculateAverage(student.getFirstGrade(), student.getSecondGrade(), student.getThirdGrade());
+		student.calculateAverage();
 		
-		assertEquals(student.calculateAverage(student.getFirstGrade(), student.getSecondGrade(), student.getThirdGrade()),averageResult,0);
+		assertEquals(student.getAverageGrade(),averageResult,0);
 		
 	}
 	
